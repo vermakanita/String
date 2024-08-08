9eb16a1894914ab2b07c9eb1a8c200b8032cbd78
KANITA VERMA <vermakanita13@gmail.com>
2024-08-08 13:58:16 +0530

    Add files via upload

---
+++ LastCharPrint.java
@@ -0,0 +1,16 @@
+/**
+ * LastCharPrint
+ */
+    class LastCharPrint {
+
+    public static void main(String[] args) {
+        String s = "kanita";
+        char sh=' ';
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+             sh=ch;
+          }
+         System.out.println();
+         System.out.println(sh );
+}
+}
