b9fecf8cc4ab4a6daa91af00369dd0a2b1bd5438
KANITA VERMA <vermakanita13@gmail.com>
2024-08-10 00:24:37 +0530

    Add files via upload

---
+++ concate2string.java
@@ -0,0 +1,12 @@
+ 
+     class String_str1 {
+        public static void main(String[] args) {
+    
+         //Concatenate two strings "Hello" and "World".
+        String str1="Hello";
+        String str2="World";
+    
+        String str3=str1.concat(str2);
+        System.out.println(str1.concat(str2));
+    
+}}
