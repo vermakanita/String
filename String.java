66bce261e9b88f335c0c19684dc191e8557bf81a
KANITA VERMA <vermakanita13@gmail.com>
2024-09-22 23:49:28 +0530

    Add files via upload

---
+++ String.java
@@ -0,0 +1,9 @@
+ class String_7strobj
+ {
+    public static void main(String[] args) 
+    {
+        String s = new String("Stephen Edwin King");
+        String s2 = new String("Stephen Edwin King");
+        System.out.println(s+" "+s2);
+    }
+}
